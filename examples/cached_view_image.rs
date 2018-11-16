627acfd5fd1696e65799f54afd19956de17df666
Magic Len <len@magiclen.org>
2018-11-17 05:08:36 +0800

    initial commit

---
+++ examples/cached_view_image.rs
@@ -0,0 +1,49 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+extern crate rocket_mongo_file_center_raw_response;
+
+extern crate validators;
+
+use std::path::Path;
+
+use rocket_mongo_file_center_raw_response::{FileCenterRawResponse, EtagIfNoneMatch};
+use rocket_mongo_file_center_raw_response::mongo_file_center::{FileCenter, FileCenterError};
+
+use rocket::request::State;
+
+use validators::short_crypt_url_component::ShortCryptUrlComponent;
+
+const HOST: &str = "localhost";
+const PORT: u16 = 27017;
+
+#[get("/<id_token>")]
+fn view(etag_if_none_match: EtagIfNoneMatch, file_center: State<FileCenter>, id_token: ShortCryptUrlComponent) -> Result<FileCenterRawResponse, FileCenterError> {
+    let id = file_center.decrypt_id_token(id_token.get_short_crypt_url_component())?;
+
+    let id_token = id_token.into_string();
+
+    let etag = FileCenterRawResponse::create_etag_by_id_token(id_token);
+
+    FileCenterRawResponse::from_object_id(file_center.inner(), etag_if_none_match, etag, &id, None::<String>)
+}
+
+fn main() {
+    let database = "test_rocket_mongo_file_center_raw_response";
+
+    let file_center = FileCenter::new(HOST, PORT, database).unwrap();
+
+    let path = Path::join(Path::new("examples"), Path::join(Path::new("images"), "image(貓).jpg"));
+
+    let file = file_center.put_file_by_path(path, None::<String>, Some(mime::IMAGE_JPEG)).unwrap();
+
+    let id_token = file_center.encrypt_id(file.get_object_id());
+
+    println!("The ID token is: {}", id_token);
+
+    println!();
+
+    rocket::ignite().manage(file_center).mount("/", routes![view]).launch();
+}
---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+/*!
+# File Center Raw Response on MongoDB for Rocket Framework
+
+This crate provides response struct used for responding raw data from the File Center on MongoDB with **Etag** cache.
+
+See `examples`.
+*/
+
+pub extern crate mime;
+pub extern crate mongo_file_center;
+extern crate mime_guess;
+extern crate percent_encoding;
+extern crate rocket;
+extern crate rocket_etag_if_none_match;
+
+use std::io::Cursor;
+use std::fmt::{self, Debug, Formatter};
+
+use mongo_file_center::{FileCenter, FileItem, FileData, FileCenterError, bson::oid::ObjectId};
+
+pub use rocket_etag_if_none_match::{EntityTag, EtagIfNoneMatch};
+
+use rocket::response::{self, Response, Responder};
+use rocket::request::Request;
+use rocket::http::{Status, hyper::header::ETag};
+
+/// The response struct used for responding raw data from the File Center on MongoDB with **Etag** cache.
+pub struct FileCenterRawResponse {
+    /// If it is `None`, that means the **Etag** is well-matched.
+    pub file_item: Option<FileItem>,
+    pub etag: EntityTag,
+    pub file_name: Option<String>,
+}
+
+impl Debug for FileCenterRawResponse {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_fmt(format_args!("FileCenterRawResponse {{etag: {:?}, file_name: {:?}, file_item: {:?}}}", self.etag, self.file_name, self.file_item))
+    }
+}
+
+impl Responder<'static> for FileCenterRawResponse {
+    fn respond_to(self, _: &Request) -> response::Result<'static> {
+        let mut response = Response::build();
+
+        match self.file_item {
+            Some(file_item) => {
+                response.header(ETag(self.etag));
+
+                if let Some(file_name) = self.file_name {
+                    if !file_name.is_empty() {
+                        response.raw_header("Content-Disposition", format!("inline; filename*=UTF-8''{}", percent_encoding::percent_encode(file_name.as_bytes(), percent_encoding::QUERY_ENCODE_SET)));
+                    }
+                } else {
+                    let file_name = file_item.get_file_name();
+                    if !file_name.is_empty() {
+                        response.raw_header("Content-Disposition", format!("inline; filename*=UTF-8''{}", percent_encoding::percent_encode(file_name.as_bytes(), percent_encoding::QUERY_ENCODE_SET)));
+                    }
+                }
+
+                response.raw_header("Content-Type", file_item.get_mime_type().to_string())
+                    .raw_header("Content-Length", file_item.get_file_size().to_string());
+
+                match file_item.into_file_data() {
+                    FileData::Collection(v) => {
+                        response.sized_body(Cursor::new(v));
+                    }
+                    FileData::GridFS(g) => {
+                        response.streamed_body(g);
+                    }
+                }
+            }
+            None => {
+                response.status(Status::NotModified);
+            }
+        }
+
+        response.ok()
+    }
+}
+
+impl FileCenterRawResponse {
+    /// Create a `FileCenterRawResponse` instance from a file item.
+    pub fn from_object_id<S: Into<String>>(file_center: &FileCenter, etag_if_none_match: EtagIfNoneMatch, etag: EntityTag, id: &ObjectId, file_name: Option<S>) -> Result<FileCenterRawResponse, FileCenterError> {
+        let is_etag_match = etag_if_none_match.weak_eq(&etag);
+
+        if is_etag_match {
+            Ok(FileCenterRawResponse {
+                file_item: None,
+                etag,
+                file_name: None,
+            })
+        } else {
+            let file_item = file_center.get_file_item_by_id(id)?;
+
+            let file_name = file_name.map(|file_name| file_name.into());
+
+            Ok(FileCenterRawResponse {
+                file_item,
+                etag,
+                file_name,
+            })
+        }
+    }
+
+    /// Given an **id_token**, and turned into an `EntityTag` instance.
+    pub fn create_etag_by_id_token<S: Into<String>>(id_token: S) -> EntityTag {
+        EntityTag::new(true, id_token.into())
+    }
+}
